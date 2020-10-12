6ad309b5a9553cb0a856c0ec1f5f36bc127e7aaf
Nikola <nik.7377gmail.com>
2020-10-12 16:07:11 +0200

    e8 and e9

---
+++ Nikola/e9/scripte9.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    document.getElementById("1").style.backgroundColor ="green";
+    document.getElementById("1").style.textAlign = "center";
+    document.getElementById("1").style.color ="yellow";
+
+    document.getElementsByTagName("p")[1].style.color ="yellow"; 
+    console.table(document.getElementsByTagName("p"));
+    
+  }
+
+  
