e2d8419188aa25c6ef646acbfb01d354b00f13dd
Scheyk <kathrin.dostal@gmail.com>
2020-10-12 11:09:04 +0200

    finish with task 1,2,3,4

---
+++ kathi/exercise3/script.js
@@ -0,0 +1,5 @@
+console.log("hi! my name is: Kathrin Schey");
+
+var greeting = "hi my name is: ";
+var name = "Kathrin Schey";
+console.log(greeting + name);
---
+++ kathi/exercise5/script.js
@@ -0,0 +1,7 @@
+let one = `Minions ipsum bappleees bananaaaa aaaaaah. Gelatooo ti aamoo! Underweaaar daa potatoooo. Underweaaar ti aamoo! Ti aamoo! Gelatooo butt bananaaaa. La bodaaa me want bananaaa! Wiiiii aaaaaah aaaaaah hahaha tank yuuu! Bappleees aaaaaah tulaliloo hana dul sae tulaliloo jeje tulaliloo jeje.`;
+
+let two = `Hana dul sae daa me want bananaaa! Ti aamoo! Hana dul sae jeje gelatooo jiji. Tatata bala tu bappleees po kass pepete bee do bee do bee do pepete jeje. Baboiii daa bananaaaa poulet tikka masala. Chasy potatoooo po kass hana dul sae. Tatata bala tu tulaliloo me want bananaaa! Tatata bala tu poulet tikka masala jiji hana dul sae. Po kass underweaaar potatoooo pepete gelatooo gelatooo potatoooo tulaliloo pepete daa. Tatata bala tu belloo! Aaaaaah aaaaaah jeje. Poulet tikka masala tulaliloo jiji jiji poopayee.`;
+
+let three = `Ti aamoo! para tú jeje poopayee butt potatoooo belloo! Potatoooo ti aamoo! Baboiii. Me want bananaaa! gelatooo potatoooo wiiiii wiiiii baboiii hahaha ti aamoo! Aaaaaah gelatooo tatata bala tu. Ti aamoo! chasy para tú wiiiii potatoooo uuuhhh. Ti aamoo! potatoooo belloo! Bananaaaa hana dul sae potatoooo poopayee bananaaaa para tú aaaaaah. Tank yuuu! tulaliloo chasy pepete po kass baboiii aaaaaah belloo! Underweaaar.`;
+
+document.querySelector(".one").style.backgroundColor = "blue";
