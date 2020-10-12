9a57e7f39ca83b1b953b7d96ff1081739d6c279f
Nikola <nik.7377gmail.com>
2020-10-12 11:56:48 +0200

    e5

---
+++ Nikola/e5/script.js
@@ -0,0 +1,6 @@
+var p1t = "How to open the devtools";
+var p2t = "depending on what browser you are using";
+var p3t = "The devtools live inside your browser in a subwindow";
+function  myFunction(){
+document.getElementById("p1").innerHTML = p1t;
+}
