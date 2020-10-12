79b05d95936abc52cd35d42a8404451c7e4e44ee
Nikola <nik.7377gmail.com>
2020-10-12 18:06:13 +0200

    some comments

--- Nikola/e9/scripte9.js
+++ Nikola/e9/scripte9.js
@@ -1,7 +1,7 @@
 function myFunction(){
     document.getElementById("1").style.backgroundColor ="green";
     document.getElementById("1").style.textAlign = "center";
-    document.getElementById("1").style.color ="yellow";
+    document.getElementById("1").style.color ="yellow"; //style="xxx"
 
     document.getElementsByTagName("p")[1].style.color ="yellow"; 
     console.table(document.getElementsByTagName("p"));
