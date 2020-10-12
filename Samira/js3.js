583cc7e4cb5d76adc697c8cb9450435ebd39ce9a
samira khadem <samira.khademhoseini@gmail.com>
2020-10-12 12:05:33 +0200

    samira folder created

---
+++ Samira/js3.js
@@ -0,0 +1,9 @@
+//console.log("connected");
+var br = "<br>";
+var hr = "<hr>";
+var phrase = "Hi my name is ";
+var myName = "Samira";
+var fullName = phrase + myName;
+console.log(fullName)
+document.write(fullName+br)
+document.write(fullName+hr)
